42311528e7e26f86ef6ff8be0a82d23476d35353
Lea Hohmann <lhohmann@CBH.KTH.SE>
2020-04-15 17:25:34 +0200

    Worked on the delay generator control GUI

--- BNC/delaycomm.py
+++ BNC/delaycomm.py
@@ -1,9 +1,40 @@
 import serial
+import tkinter as tk
 
-ser = serial.Serial('COM5',115200, timeout=1)
 
-ser.write(":PULS2:DEL?")
-delayB = ser.readline()
-print(delayB)
+ser = serial.Serial('COM5',baudrate=115200, bytesize=8, parity='N', stopbits=1, timeout=1)
 
-ser.close()
+def updatechannels():
+    ser.write(b':PULS2:DEL?\r\n')
+    lastline = ser.readline().decode("utf-8")
+    delayB = lastline[:13]
+    ChB.configure(text="Channel B delay: {}".format(delayB))
+
+def changedelay(channumber):
+    newdelay = Bfield.get()
+    inputstring = "PULS{}:DEL {}\r\n".format(channumber,newdelay)
+    ser.write(inputstring.encode("utf-8"))
+    lastline = ser.readline().decode("utf-8")
+    updatechannels()
+
+def quitgui():
+    ser.close()
+    root.quit()
+
+root = tk.Tk()
+
+status = tk.Button(root, text="Status", command=updatechannels)
+status.pack()
+
+ChB = tk.Label(root, text="Channel B delay: 0")
+ChB.pack()
+Bfield = tk.Entry(root)
+Bfield.pack()
+Bchange = tk.Button(root, text="Set", command=lambda:changedelay(2))
+Bchange.pack()
+
+quit = tk.Button(root, text="Quit", command=quitgui)
+quit.pack()
+
+root.mainloop()
+
