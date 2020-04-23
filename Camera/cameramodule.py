2592ba25ec18c905968e9a2313a26e760907d988
Lea Hohmann <lea.hohmann@outlook.de>
2020-04-23 13:48:41 +0200

    Started on cameramodule class

--- Camera/cameramodule.py
+++ Camera/cameramodule.py
@@ -1,51 +1,70 @@
 import PySpin
 import tkinter as tk
 
+
+class CameraApp(tk.Frame):
+
+    def __init__(self,root,serialnumber):
+        tk.Frame.__init__(self,root)
+        self.pack()
+
+        self.root = root
+        self.camera = ""
+        self.serialnumber = serialnumber
+
+        self.connect = tk.Button(self, Text="Connect to camera", command=)
+        self.connect.pack(side=tk.TOP, ipadx=5, ipady=5, padx=5, pady=5)
+
+        self.message = tk.Label(self, Text="")
+        self.message.pack(side=tk.BOTTOM, padx=5)
+
+
+    def connectcamera(self):
+        self.system = PySpin.System.GetInstance()
+        self.cameralist = system.GetCameras()
+
+        if self.cameralist.GetSize() == 0:
+            self.message.configure(Text="No cameras connected. Connect a camera and try again")
+            self.cameralist.Clear()
+            self.system.ReleaseInstance()
+
+        else:
+            self.cameraident()
+            self.cameralist.Clear()
+            
+            if self.camera == "":
+                self.message.configure(Text="Camera not found. Connect the camera and try again")
+                self.system.ReleaseInstance()
+            else:
+                self.guiinit()
+
+    def cameraident(self):
+        for cam in self.cameralist:
+
+            device_nodemap = cam.GetTLDeviceNodeMap()
+            node_serialno = PySpin.CStringPtr(device_nodemap.GetNode("DeviceSerialNumber"))
+            if PySpin.IsAvailabla(node_serialno) and PySpin.IsReadable(node_serialno):
+                serialno = node_serialno.ToString()
+            else:
+                serialno = "0"
+
+            if serialno == self.serialnumber:
+                self.camera = cam
+                self.message.configure(Text="Connected to camera")
+                self.nodemap = device_nodemap
+                break
+    
+        del cam
+
+
+    def guiinit(self):
+
+
+
+
 def quitthecamera(system):
     global camera
     camera =""
     system.ReleaseInstance()
 
-def cameraident(cameralist,serialnumber):
-
-    for cam in cameralist:
-
-        device_nodemap = cam.GetTLDeviceNodeMap()
-
-        node_serialno = PySpin.CStringPtr(device_nodemap.GetNode("DeviceSerialNumber"))
-        if PySpin.IsAvailable(node_serialno) and PySpin.IsReadable(node_serialno):
-            serialno = node_serialno.ToString()
-        else:
-            serialno = "0"
-
-        if serialno == serialnumber:
-            global camera
-            camera = cam
-            message = "Connected to camera"
-            print(message)
-            break
-    
-    import pdb; pdb.set_trace()
-    
-    if camera == "":
-        message = "Camera not found"
-        print(message)
-
-    del cam
-    return message
-        
-
-def camerainit(system,serialnumber):
-
-    cameralist = system.GetCameras()
-
-    if cameralist.GetSize() == 0:
-        message = "No cameras connected. Connect a camera and try again."
-        print(message)
-        cameralist.Clear()
-
-    else:
-        cameraident(cameralist,serialnumber)
-
-    cameralist.Clear()
 
